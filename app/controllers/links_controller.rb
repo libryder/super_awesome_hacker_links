7a93aa1744c441b38248ce1f5b74538ba53f0704
Brian Bauer <brian.bauer@g5platform.com>
2012-04-13 13:44:34 -0700

    Remove unused actions

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,16 +3,8 @@ class LinksController < ApplicationController
     @links = LinkDecorator.all.sort_by { |l| l.votes.size.to_i }.reverse
   end
 
-  def show
-    @link = Link.find(params[:id])
-  end
-
   def new
     @link = Link.new
-  end
-
-  def edit
-    @link = Link.find(params[:id])
   end
 
   def create
@@ -22,43 +14,6 @@ class LinksController < ApplicationController
       redirect_to links_path, notice: 'Link was successfully created.'
     else
       render action: "new"
-    end
-  end
-
-  def update
-    @link = Link.find(params[:id])
-
-    if @link.update_attributes(params[:link])
-      redirect_to @link, notice: 'Link was successfully updated.'
-    else
-      render action: "edit"
-    end
-  end
-
-  def destroy
-    @link = Link.find(params[:id])
-    @link.destroy
-
-    redirect_to links_path
-  end
-
-  def rate
-    link = Link.find(params[:id])
-    user_session[:voted_links] = [] if user_session[:voted_links].nil?
-
-    if current_user.id == link.user.id
-      flash[:error] = "You Can't Vote On Your Own Link!"
-      redirect_to links_path
-    else
-      if user_session[:voted_links] && user_session[:voted_links].include?(link.id)
-        flash[:error] = "You've already voted on this link!"
-        redirect_to links_path
-      else
-        link.increment!(:rating)
-        user_session[:voted_links] << link.id
-        flash[:notice] = "Thanks for voting!"
-        redirect_to links_path
-      end
     end
   end
 end
--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -16,24 +16,10 @@ describe LinksController do
     end
   end
 
-  describe "GET show" do
-    it "assigns the requested link as @link" do
-      get :show, {:id => @link.to_param}
-      assigns(:link).should eq(@link)
-    end
-  end
-
   describe "GET new" do
     it "assigns a new link as @link" do
       get :new
       assigns(:link).should be_a_new(Link)
-    end
-  end
-
-  describe "GET edit" do
-    it "assigns the requested link as @link" do
-      get :edit, {:id => @link.to_param}
-      assigns(:link).should eq(@link)
     end
   end
 
@@ -70,55 +56,6 @@ describe LinksController do
         post :create, {:link => {}}
         response.should render_template("new")
       end
-    end
-  end
-
-  describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested link" do
-        Link.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, {:id => @link.to_param, :link => {'these' => 'params'}}
-      end
-
-      it "assigns the requested link as @link" do
-        put :update, {:id => @link.to_param, :link => valid_attributes}
-        assigns(:link).should eq(@link)
-      end
-
-      it "redirects to the link" do
-        put :update, {:id => @link.to_param, :link => valid_attributes}
-        @link.update_attributes valid_attributes
-        response.should redirect_to(@link)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns the link as @link" do
-        link = Link.create! valid_attributes
-        Link.any_instance.stub(:save).and_return(false)
-        put :update, {:id => link.to_param, :link => {}}
-        assigns(:link).should eq(link)
-      end
-
-      it "re-renders the 'edit' template" do
-        link = Link.create! valid_attributes
-        Link.any_instance.stub(:save).and_return(false)
-        put :update, {:id => link.to_param, :link => {}}
-        response.should render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE destroy" do
-    it "destroys the requested link" do
-      expect {
-        delete :destroy, {:id => @link.to_param}
-      }.to change(Link, :count).by(-1)
-    end
-
-    it "redirects to the links list" do
-      delete :destroy, {:id => @link.to_param}
-      response.should redirect_to(links_url)
     end
   end
 end
